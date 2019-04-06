626d59e6d2c0976ac12c0d809b9fb5d4e572bd84
Patrick Kim <pkim050@gmail.com>
2019-04-06 14:43:14 +0000

    Automatically backed up by Learn

--- db/migrate/20190406144131_create_users.rb
+++ db/migrate/20190406144131_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |element|
       element.string :name
-      element.email :email
+      element.string :email
       element.string :password
     end
   end
